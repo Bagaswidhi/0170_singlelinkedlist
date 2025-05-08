015cc6e8ce6642098fc555ca9ae22a6d9bc396ae
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-05-08 11:30:40 +0700

    Menambahkan tampilan Switch case 4

--- Single-linkedList.cpp
+++ Single-linkedList.cpp
@@ -161,6 +161,29 @@ int main()
             mhs.traverse();
         }
         break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "Masukkan No. Mahasiswa yang dicari: ";
+            cin >> nim;
+            if (mhs.Search(nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa : " << current->noMhs << endl;
+                cout <<"\n";
+            }
+        }
+        break;
         }
     }
 }
