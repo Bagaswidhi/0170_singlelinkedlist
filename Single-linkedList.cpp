f479df919e9515ee761d091c77dd84397d9bebaa
Bagaswidhi <Bagaswidhi10@gmail.com>
2025-05-08 11:15:42 +0700

    Menambahkan method Travers List empty pada class Single linked list

--- Single-linkedList.cpp
+++ Single-linkedList.cpp
@@ -91,6 +91,24 @@ public:
         return true;
     }
 
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah: \n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
 };
 
 int main()
